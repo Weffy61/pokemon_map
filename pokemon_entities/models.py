a582ca536be28b9efedbd3f657ef8d7f9418cba0
Weffy61 <weffy@mail.ru>
2023-09-25 19:30:35 +0300

    show Pokemon name in admin UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = "Pokemon"
         verbose_name_plural = "Pokemons"
