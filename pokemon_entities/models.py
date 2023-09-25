2dcd0d4ebf73db743a8e2db3262af66b3019dc76
Weffy61 <weffy@mail.ru>
2023-09-25 19:00:44 +0300

    register Pokemon model on admin UI

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,3 +3,7 @@ from django.db import models
 
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name = "Pokemon"
+        verbose_name_plural = "Pokemons"
