74cdb63959c3f020519ab6e6c528bcd911eb64f5
Weffy61 <weffy@mail.ru>
2023-09-29 20:02:54 +0300

    Delete extra false in field arguments

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Pokemon(models.Model):
     title_ru = models.CharField(max_length=200, verbose_name='Имя покемона')
-    title_en = models.CharField(max_length=200, null=False, blank=True, verbose_name='Имя покемона на английском')
-    title_jp = models.CharField(max_length=200, null=False, blank=True, verbose_name='Имя покемона на японском')
+    title_en = models.CharField(max_length=200, blank=True, verbose_name='Имя покемона на английском')
+    title_jp = models.CharField(max_length=200, blank=True, verbose_name='Имя покемона на японском')
     image = models.ImageField(upload_to='images/', null=True, blank=True, verbose_name='Изобрадение')
-    descr = models.TextField(max_length=400, null=False, blank=True, verbose_name='Описание')
+    descr = models.TextField(max_length=400, blank=True, verbose_name='Описание')
     previous_evolution = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True,
                                            related_name='next_evolutions', verbose_name='Предыдущее поколение покемона')
 
